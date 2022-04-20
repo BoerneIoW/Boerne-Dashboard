37f4ec48164665642d01b34485c5736c465668e7
vrueda97 <99679791+BoerneIoW@users.noreply.github.com>
2022-04-20 11:47:36 -0500

    update

--- boerne-water-supply/rcode/use1_water_quality_data.R
+++ boerne-water-supply/rcode/use1_water_quality_data.R
@@ -5,6 +5,15 @@
 # MARCH 2022
 #
 ###################################################################################################################################################
+
+######################################################################################################################################################################
+#
+#   Load old data
+#
+######################################################################################################################################################################
+old.data <- read.csv(paste0(swd_data, "quality/boerne_water_quality.csv"))
+boerne_quality_sites <- read_sf(paste0(swd_data, "quality/water_quality_sites.geojson"))
+
 ######################################################################################################################################################################
 #
 #   READ IN QUALITY SITES AND DATA: Texas Stream Team Water Quality Data
@@ -34,23 +43,16 @@ boerne_data <- boerne_data[-c(9,11:12,21)]
 boerne_data <- as.data.frame(boerne_data)
 boerne_data <- boerne_data %>% arrange(site_id, date)
 
-# limit data to 2021
+# limit data to 2022
 boerne_data <- boerne_data %>% mutate(year = year(date))
-boerne_data <- boerne_data %>% filter(year <= 2021)
+new_boerne_data <- boerne_data %>% filter(year >= 2022)
+
+#combine new and old data
+all_boerne_data <- rbind(old.data, new_boerne_data)
 
 #save out
-write.csv(boerne_data, paste0(swd_data, "quality/boerne_water_quality.csv"), row.names=FALSE)
+write.csv(all_boerne_data, paste0(swd_data, "quality/all_boerne_water_quality.csv"), row.names=FALSE)
 
-# create metadata to generate geojson file
-  #boerne_quality_sites <- boerne_data %>% slice(1,37,226,320,494,590,639,717,736,737,742) #choose one observation per site to extract metadata from
-  #boerne_quality_sites <- boerne_quality_sites[-c(7:18)]
-  #zt <- st_as_sf(boerne_quality_sites, coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
-  #mapview::mapview(zt)
-  #geojson_write(boerne_quality_sites, file =  paste0(swd_data, "quality/water_quality_sites.geojson"))
-
-#load geojson file
-boerne_quality_sites <- read_sf(paste0(swd_data, "quality/water_quality_sites.geojson"))
-mapview::mapview(boerne_quality_sites)
 ################################################################################################################################################################
 # remove all except for global environment 
 rm(list= ls()[!(ls() %in% c('julian.ref','update.date', 'current.month', 'current.year', 'end.date', 'end.year', 
