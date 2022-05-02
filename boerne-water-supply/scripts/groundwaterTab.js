993a6a751782d4509158c1f62c98159d9d3b2fdb
vrueda97 <99679791+BoerneIoW@users.noreply.github.com>
2022-05-02 07:19:50 -0500

    update

--- boerne-water-supply/scripts/groundwaterTab.js
+++ boerne-water-supply/scripts/groundwaterTab.js
@@ -22,6 +22,7 @@ function toggleGWStats(target){
 function createGWTab(gwID, recentDate, gwPlotType) {
   document.getElementById('gwPlot2').innerHTML = "";
   document.getElementById('relGWTitle').innerHTML = "Groundwater Levels Relative to Historic Levels";
+  document.getElementById('monthlyGWTitle').innerHTML = "Long-term Monthly Trends";
   document.getElementById('longGWTitle').innerHTML = "Long-term Annual Trends";
 //parse date to scale axis
 parseDate = d3.timeParse("%Y-%m-%d");
@@ -283,6 +284,170 @@ var gwlayout3 = {
 };
 var data3 = [traceAnnual];
 Plotly.newPlot('gwPlot3', data3, gwlayout3, config);
-});//end D3
+  });//end D3
 
 } //END CREATE CHART FUNCTION ##########################################################
+
+//#####################################################################################
+//         create plot for average monthly observations
+//#####################################################################################//
+
+
+function plotGroundwater(gwID, checked) {
+  document.getElementById("gwMonthPlot").innerHTML = ""; //set blank plot
+  //parse date to scale axis
+  parseDate = d3.timeParse("%Y-%m-%d");
+
+  d3.csv("data/gw/all_boerne_monthly_avg.csv").then(function (dfgw) {
+      dfgw.forEach(function (d) {
+          d.month = +d.month;
+          d.year = +d.year;
+          d.mean_depth_ft = +d.mean_depth_ft;
+      }); //end for each
+      var selgw = dfgw.filter(function (d) {
+          return d.site === gwID;
+      });
+
+      //create a trace for each year
+      var data = [];
+      var xMonth = [
+          "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+      ];
+      var yOther;
+      var OtherYearTrace;
+      //draw the traces for all years but current
+      var xYear = [];
+      var minYear = d3.min(selgw.map(function (d) {  return d.year; }) );
+      for (var i = minYear; i <= 2022; i++) {
+          xYear.push(i);
+      }
+
+      var showLegVal = true;
+      for (i = 0; i < xYear.length - 1; i++) {
+          tempSelect = xYear[i];
+          temp = selgw.filter(function (d) {
+              return d.year === tempSelect;
+          });
+          tempName = "Median Depth (ft): %{y:.2f} in %{x}, " + tempSelect;
+          yOther = temp.map(function (d) {
+              return d.mean_depth_ft;
+          });
+          //create individual trace
+          if (i > 0) {
+              showLegVal = false;
+          }
+          OtherYearTrace = {
+              x: xMonth,
+              y: yOther,
+              mode: "lines",
+              type: "scatter",
+              hovertemplate: tempName,
+              opacity: 0.4,
+              line: { color: "#c5c5c5", width: 1 }, //light gray
+              name: "years",
+              showlegend: showLegVal,
+          };
+          //push trace
+          data.push(OtherYearTrace);
+      } // end for loop
+
+      //draw other selected years
+      var selectYears;
+      var selectTraces;
+      var colorLineAll = [
+          "rgb(26,121,131)",
+          "#567258",
+          "#bf9f4c",
+          "#b9b59f",
+          "#6f634d",
+          "#314837",
+          "#b0d76f",
+          "#0ed0d0",
+          "#246f8f",
+          "5234578",
+          "#900909",
+          "#d16014",
+          "#58381f",
+      ];
+
+      for (i = 0; i < checked.length; i++) {
+          tempSelect = Number(checked[i]);
+          selectYears = selgw
+              .filter(function (d) {
+                  return d.year === tempSelect;
+              })
+              .map(function (d) {
+                  return d.mean_depth_ft;
+              });
+          tempName = "Median Depth (ft): %{y:.2f} in %{x}, " + tempSelect;
+          colorLine = colorLineAll[i];
+          if (tempSelect === 2011) { colorLine = "red"; } // highlight drought years
+          //if (tempSelect === 2007) { colorLine = "darkred"; } // highlight drought years
+          //if (tempSelect === 2008) { colorLine = "orange"; } // highlight drought years
+          //if (tempSelect === 2003 || tempSelect === 2018){ colorLine = "blue"; } // highlight wettest years on record
+
+          selectTraces = {
+              x: xMonth,
+              y: selectYears,
+              mode: "lines+markers",
+              type: "scatter",
+              hovertemplate: "Median Depth (ft): %{y:.2f} in %{x}, " + tempSelect,
+              opacity: 1,
+              marker: { color: colorLine, size: 6 },
+              line: { color: colorLine, width: 2 },
+              name: tempSelect,
+              showlegend: true,
+          };
+          data.push(selectTraces);
+      }
+
+      //draw 2022 year
+      var yCurrent = selgw
+          .filter(function (d) {
+              return d.year === currentYear;
+          })
+          .map(function (d) {
+              return d.mean_depth_ft;
+          });
+      var trace2022 = {
+          x: xMonth,
+          y: yCurrent,
+          mode: "lines+markers",
+          type: "scatter",
+          hovertemplate: "Median Depth (ft): %{y:.2f} in %{x}, " + currentYear,
+          opacity: 1,
+          marker: { color: "black", size: 6 },
+          line: { color: "black", width: 3 },
+          name: currentYear,
+          showlegend: true,
+      };
+      data.push(trace2022);
+
+      //PLOT CHART
+      var layout = {
+          yaxis: {
+              title: "Average Monthly Feet Below Surface (ft)",
+              titlefont: { color: "rgb(0, 0, 0)", size: 14 },
+              tickfont: { color: "rgb(0, 0, 0)", size: 12 },
+              showline: false,
+              showgrid: false,
+              showticklabels: true,
+              range: [0, 30],
+          },
+          xaxis: {
+              showline: false,
+              showgrid: false,
+              showticklabels: true,
+              title: "",
+              titlefont: { color: "rgb(0, 0, 0)", size: 14 },
+              tickfont: { color: "rgb(0, 0, 0)", size: 12 },
+          },
+          hovermode: "closest",
+          height: 240,
+          showlegend: true,
+          legend: { x: 1, y: 1, xanchor: "left", font: { size: 10 } },
+          margin: { t: 30, b: 30, r: 10, l: 35 },
+      };
+      Plotly.newPlot("gwMonthPlot", data, layout, config);
+  }); //end d3
+} //end plotGW function
