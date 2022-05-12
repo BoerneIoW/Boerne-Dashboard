80674c3092921e75a6c92f712ffd14dceb903c6b
vrueda97 <99679791+BoerneIoW@users.noreply.github.com>
2022-05-12 12:11:46 -0500

    update

--- boerne-water-supply/scripts/groundwaterTab.js
+++ boerne-water-supply/scripts/groundwaterTab.js
@@ -289,69 +289,197 @@ Plotly.newPlot('gwPlot3', data3, gwlayout3, config);
 //#####################################################################################//
 
 
-d3.csv("data/gw/all_boerne_monthly_avg.csv").then(function(gwMonthly){
-  
-  gwMonthly.forEach(function(d){
-    d.monthlyflow = +d.mean_depth_ft;
-    d.month = +d.month;
+function createTraceGW(target) {
+  checkedGW = [];
+  $("input[name='checkGWYear']:checked").each(function () {
+      checkedGW.push($(this).val());
   });
 
-  var selGWMonthly = gwMonthly.filter(function(d){return d.site === gwID.toString(); });
-  var xMonth = selGWMonthly.map(function(d) {return d.month; });
-  var yDepth = selGWMonthly.map(function(d) {return d.monthlyflow; });
-  var minVal = Math.max(yDepth);
+  Plotly.purge("gwPlot4");
+  plotGroundwater(gwID, checkedGW);
+  return checkedGW;
+}
 
-  traceMonthly = {
-    type: 'scatter',
-    x: xMonth,  y: yDepth,
-    text: "median Depth",
-    mode: 'lines+markers',
-    name: 'Median Monthly Water Levels',
-    marker: { color: "black", size: 5, opacity: 0.8},
-    line: { color: 'gray',  width: 1},
-    hovertemplate:
-    "Median Depth (ft): %{y:.2f} in %{x}"
-  };
-  var gwlayout4 = {
-    yaxis: {
-      title: "Feet below surface (ft)",
-      titlefont: {color: 'rgb(0, 0, 0)', size: 14},
-      tickfont: {color: 'rgb(0, 0, 0)', size: 12},
-      showline: true,
-      showgrid: false,
-      range: [0, minVal],
-      autorange: "reversed"
-    },
-    xaxis: {
-      showline: true,
-      title: '',
-      titlefont: {color: 'rgb(0, 0, 0)', size: 14},
-      tickfont: {color: 'rgb(0, 0, 0)', size: 12},
-    },
-    height: 300,
-    showlegend: true,
-    legend: {x: 0, y: 0.95, xanchor: 'left', yanchor: 'right'},
-    margin: {t: 30, b: 30, r: 30, l: 50 },
-    shapes: [{
-      type: 'line', xref: 'paper', yref: 'y',
-      x0: 0, x1: 1, y0:0, y1: 0,
-      line: {color: "#745508", width: "2"}
-    }],
-    annotations: [
-      //above ground
-      { xref: 'paper', yref: 'paper', //ref is assigned to x values
-        x: 0.6, y: 1,
-        xanchor: 'left', yanchor: 'top',
-        text: "Land Surface", 
-        font: {family: 'verdana', size: 11, color: '#745508'},
-        showarrow: false,
+function plotGroundwater(gwID, checkedGW) {
+  
+  document.getElementById("gwPlot4").innerHTML = ""; //set blank plot
+  //parse date to scale axis
+  parseDate = d3.timeParse("%Y-%m-%d");
+
+  d3.csv("data/gw/all_boerne_monthly_avg.csv").then(function (gwMonthly) {
+      
+    gwMonthly.forEach(function (d) {
+      d.mean_depth_ft = +d.mean_depth_ft;
+      d.month = +d.month;
+      d.year = +d.year;
+    });
+
+    var selGWMonthly = gwMonthly.filter(function(d){
+      return d.site === gwID.toString(); 
+    });
+    //console.log(selGWMonthly)
+
+    // create a trace for each year
+    var data4 = [];
+    //var xMonth = selGWMonthly.map(function(d) {return d.month; });
+    var xMonth = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+    ];
+
+    var yOther = [];
+    var OtherYearTrace;
+
+    //draw the traces for all years but current
+    var xYear = [];
+    var minYear = d3.min(selGWMonthly.map(function (d) {  return d.year; }) );
+    for (var i = minYear; i <= 2022; i++) {
+      xYear.push(i);
+    }
+    var showLegVal = true;
+    for (i = 0; i < xYear.length - 1; i++) {
+      tempSelect = xYear[i];
+      temp = selGWMonthly.filter(function (d) {
+        return d.year === tempSelect;
+      });
+      tempName = "%{y:.1f} ft in " + tempSelect;
+      yOther = temp.map(function (d) {
+        return d.mean_depth_ft;
+      });
+      //create individual trace
+      if (i > 0) {
+        showLegVal = false;
       }
-    ]
-  };
-  var data4 = [traceMonthly];
-  Plotly.newPlot('gwPlot4', data4, gwlayout4, config);
-    });//end D3
+      OtherYearTrace = {
+        x: xMonth,
+        y: yOther,
+        mode: "lines",
+        type: "scatter",
+        hovertemplate: tempName,
+        opacity: 0.4,
+        line: { color: "#c5c5c5", width: 1 }, //light coral
+        name: "years",
+        showlegend: showLegVal,
+      };
+      //push trace
+      data4.push(OtherYearTrace); 
+    }// end for loop
 
-} //END CREATE CHART FUNCTION ##########################################################
+    //draw other selected years
+    var selectYears;
+    var selectTraces;
+    //set array of colors
+    var colorLineAll = [
+      "rgb(26,121,131)",
+      "#567258",
+      "#bf9f4c",
+      "#b9b59f",
+      "#6f634d",
+      "#314837",
+      "#b0d76f",
+      "#0ed0d0",
+      "#246f8f",
+      "5234578",
+      "#900909",
+      "#d16014",
+      "#58381f",
+    ];
+
+    for (i = 0; i < checkedGW.length; i++) {
+      tempSelect = Number(checkedGW[i]);
+      selectYears = selGWMonthly
+        .filter(function (d) {
+          return d.year === tempSelect;
+        })
+        .map(function (d) {
+          return d.mean_depth_ft;
+        });
+      tempName = "%{y:.1f} ft in %{x}, " + tempSelect;
+      colorLine = colorLineAll[i];
+      if (tempSelect === 2011) { colorLine = "red"; } // highlight drought years
+
+      selectTraces = {
+        x: xMonth,
+        y: selectYears,
+        mode: "lines+markers",
+        type: "scatter",
+        hovertemplate: "%{y:.1f} ft in %{x}, " + tempSelect,
+        opacity: 1,
+        marker: { color: colorLine, size: 6 },
+        line: { color: colorLine, width: 2 },
+        name: tempSelect,
+        showlegend: true,
+      };
+      data4.push(selectTraces);
+    }
+
+    //draw 2022 year
+    var yCurrent = selGWMonthly
+      .filter(function (d) {
+        return d.year === currentYear;
+      })
+      .map(function (d) {
+        return d.mean_depth_ft;
+      });
+    var trace2022 = {
+      x: xMonth,
+     y: yCurrent,
+     mode: "lines+markers",
+      type: "scatter",
+      hovertemplate: "%{y:.1f} ft in %{x}, " + currentYear,
+      opacity: 1,
+      marker: { color: "black", size: 6 },
+      line: { color: "black", width: 3 },
+      name: currentYear,
+      showlegend: true,
+    };
+    data4.push(trace2022);
+
+    //PLOT CHART
+    var gwlayout4 = {
+      yaxis: {
+        title: "Feet below surface (ft)",
+        titlefont: {color: 'rgb(0, 0, 0)', size: 14},
+        tickfont: {color: 'rgb(0, 0, 0)', size: 12},
+        showline: true,
+        showgrid: false,
+        range: [0, minVal],
+        autorange: "reversed"
+      },
+      xaxis: {
+        showline: false,
+        showgrid: true,
+        showticklabels: true,
+        tickformat: "%b-%d",
+        title: "",
+        titlefont: { color: "rgb(0, 0, 0)", size: 14 },
+        tickfont: { color: "rgb(0, 0, 0)", size: 12 },
+      },
+      hovermode: "closest",
+      height: 300,
+      showlegend: true,
+      legend: {x: 0, y: 0.95, xanchor: 'left', yanchor: 'right'},
+      margin: {t: 30, b: 30, r: 10, l: 35 },
+      shapes: [{
+        type: 'line', xref: 'paper', yref: 'y',
+        x0: 0, x1: 1, y0:0, y1: 0,
+        line: {color: "#745508", width: "2"}
+      }],
+      annotations: [
+        //above ground
+        { xref: 'paper', yref: 'paper', //ref is assigned to x values
+          x: 0.6, y: 1,
+          xanchor: 'left', yanchor: 'top',
+          text: "Land Surface", 
+          font: {family: 'verdana', size: 11, color: '#745508'},
+          showarrow: false,
+        }
+      ]
+    };
+    Plotly.newPlot("gwPlot4", data4, gwlayout4, config);
+  }); // end D3
+} //END CREATE MONTHLY CHART FUNCTION ##########################################################
+
+
+} //END CREATE OVERALL GROUNDWATER FUNCTION ##########################################################
 
 
--- boerne-water-supply/scripts/groundwaterTab_copy2.js
+++ boerne-water-supply/scripts/groundwaterTab_copy2.js
@@ -8,200 +8,185 @@ function createTraceGW(target) {
   });
 
   Plotly.purge("gwPlot4");
-  createGWInfo(gwID, checkedGW);
+  plotGroundwater(gwID, checkedGW);
   return checkedGW;
 }
 
-function createGWInfo(gwID, checkedGW) {
+function plotGroundwater(gwID, checkedGW) {
+  
+  document.getElementById("gwPlot4").innerHTML = ""; //set blank plot
+  //parse date to scale axis
+  parseDate = d3.timeParse("%Y-%m-%d");
 
-    //console.log(checkedGW);
-    //read in demand data
-    d3.csv("data/gw/all_boerne_monthly_avg.csv").then(function (gwMonthly) {
+  d3.csv("data/gw/all_boerne_monthly_avg.csv").then(function (gwMonthly) {
       
-      gwMonthly.forEach(function (d) {
-        d.monthlyflow = +d.mean_depth_ft;
-        d.month = +d.month;
-        d.year = +d.year;
+    gwMonthly.forEach(function (d) {
+      d.mean_depth_ft = +d.mean_depth_ft;
+      d.month = +d.month;
+      d.year = +d.year;
+    });
+
+    var selGWMonthly = gwMonthly.filter(function(d){
+      return d.site === gwID.toString(); 
+    });
+    //console.log(selGWMonthly)
+
+    // create a trace for each year
+    var data4 = [];
+    //var xMonth = selGWMonthly.map(function(d) {return d.month; });
+    var xMonth = [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+    ];
+
+    var yOther = [];
+    var OtherYearTrace;
+
+    //draw the traces for all years but current
+    var xYear = [];
+    var minYear = d3.min(selGWMonthly.map(function (d) {  return d.year; }) );
+    for (var i = minYear; i <= 2022; i++) {
+      xYear.push(i);
+    }
+    var showLegVal = true;
+    for (i = 0; i < xYear.length - 1; i++) {
+      tempSelect = xYear[i];
+      temp = selGWMonthly.filter(function (d) {
+        return d.year === tempSelect;
       });
+      tempName = "%{y:.1f} ft in " + tempSelect;
+      yOther = temp.map(function (d) {
+        return d.mean_depth_ft;
+      });
+      //create individual trace
+      if (i > 0) {
+        showLegVal = false;
+      }
+      OtherYearTrace = {
+        x: xMonth,
+        y: yOther,
+        mode: "lines",
+        type: "scatter",
+        hovertemplate: tempName,
+        opacity: 0.4,
+        line: { color: "#c5c5c5", width: 1 }, //light coral
+        name: "years",
+        showlegend: showLegVal,
+      };
+      //push trace
+      data4.push(OtherYearTrace); 
+    }// end for loop
 
-      var selGWMonthly = gwMonthly.filter(function(d){return d.site === gwID.toString(); });
-      //console.log(selGWMonthly)
+    //draw other selected years
+    var selectYears;
+    var selectTraces;
+    //set array of colors
+    var colorLineAll = [
+      "rgb(26,121,131)",
+      "#567258",
+      "#bf9f4c",
+      "#b9b59f",
+      "#6f634d",
+      "#314837",
+      "#b0d76f",
+      "#0ed0d0",
+      "#246f8f",
+      "5234578",
+      "#900909",
+      "#d16014",
+      "#58381f",
+    ];
 
-        
-
-      if (selGWMonthly.length > 0) {
-        document.getElementById("gwPlot4").innerHTML = ""; //set blank plot
-        var minVal = Math.max(yDepth);
-          
-        //create multiple traces
-        var data = [];
-        var xMonths = selGWMonthly.map(function(d) {return d.month; });
-        let xMonth = xMonths.filter(
-          (item, i, ar) => ar.indexOf(item) === i
-        );
-
-        //draw the traces for all years but current
-        var xYears = [];
-        for (var i = 2000; i <= currentYear; i++) {
-          xYears.push(i);
-        }
-
-        var yOther = [];
-        var OtherYearTrace;
-
-        for (i = 0; i < xYears.length - 1; i++) {
-          tempSelect = xYears[i];
-          temp = selGWMonthly.filter(function (d) {
-            return d.year === tempSelect;
-          });
-          tempName = "%{y:.1f} mgd in " + tempSelect;
-          //xDate = temp.map(function(d){ return d.date; });
-          yOther = temp.map(function (d) {
-            return d.mean_depth_ft;
-          });
-          //create individual trace
-          var showLegVal = true;
-          if (i > 0) {
-            showLegVal = false;
-          }
-
-          OtherYearTrace = {
-            x: xMonth,
-            y: yOther,
-            mode: "lines",
-            type: "scatter",
-            hovertemplate: tempName,
-            opacity: 0.4,
-            line: { color: "#c5c5c5", width: 1 }, //light coral
-            name: "years",
-            showlegend: showLegVal,
-          };
-
-          //push trace
-          data.push(OtherYearTrace);
-        } // end for loop
-
-        //draw other selected years
-        var selectYears;
-        var selectTraces;
-        //set array of colors
-        var colorLineAll = [
-          "rgb(26,121,131)",
-          "#567258",
-          "#bf9f4c",
-          "#b9b59f",
-          "#6f634d",
-          "#314837",
-          "#b0d76f",
-          "#0ed0d0",
-          "#246f8f",
-          "5234578",
-          "#900909",
-          "#d16014",
-          "#58381f",
-        ];
-        var colorLine;
-
-        for (i = 0; i < checkedGW.length; i++) {
-          tempSelect = Number(checkedGW[i]);
-          selectYears = selGWMonthly
-            .filter(function (d) {
-              return d.year === tempSelect;
-            })
-            .map(function (d) {
-              return d.mean_depth_ft;
-            });
-          tempName = "%{y:.1f} mgd in %{x}, " + tempSelect;
-          colorLine = colorLineAll[i];
-          if (tempSelect === 2011) {
-            colorLine = "red";
-          }
-          // if (tempSelect === 2007) {
-          //     colorLine = "darkred";
-          // }
-
-          selectTraces = {
-            x: xMonth,
-            y: selectYears,
-            mode: "lines",
-            type: "scatter",
-            hovertemplate: "%{y:.1f} mgd in %{x}, " + tempSelect,
-            opacity: 1,
-            line: { color: colorLine, width: 2 },
-            name: tempSelect,
-            showlegend: true,
-          };
-          data.push(selectTraces);
-        }
-
-        //draw median and selected year
-        selGWMonthlyNow = selGWMonthly.filter(function (d) {
-          return d.year === currentYear;
-        });
-        var ySelect = selGWMonthlyNow.map(function (d) {
+    for (i = 0; i < checkedGW.length; i++) {
+      tempSelect = Number(checkedGW[i]);
+      selectYears = selGWMonthly
+        .filter(function (d) {
+          return d.year === tempSelect;
+        })
+        .map(function (d) {
           return d.mean_depth_ft;
         });
+      tempName = "%{y:.1f} ft in %{x}, " + tempSelect;
+      colorLine = colorLineAll[i];
+      if (tempSelect === 2011) { colorLine = "red"; } // highlight drought years
 
-        //PLOTLY
-        var seltrace = {
-          y: ySelect,
-          x: xMonth,
-          /*marker: {
-      size: 6, color: "rgb(43,28,88",
-      line: {color: 'black', width: 1}
-      },*/
-          line: { color: "rgb(43,28,88", width: 3 },
-          mode: "lines",
-          type: "scatter",
-          name: "2022",
-          showlegend: true,
-          hovertemplate: "%{y:.1f} mgd in %{x}",
-        };
+      selectTraces = {
+        x: xMonth,
+        y: selectYears,
+        mode: "lines+markers",
+        type: "scatter",
+        hovertemplate: "%{y:.1f} ft in %{x}, " + tempSelect,
+        opacity: 1,
+        marker: { color: colorLine, size: 6 },
+        line: { color: colorLine, width: 2 },
+        name: tempSelect,
+        showlegend: true,
+      };
+      data4.push(selectTraces);
+    }
 
-        var gwlayout4 = {
-          yaxis: {
-            title: "Feet below surface (ft)",
-            titlefont: {color: 'rgb(0, 0, 0)', size: 14},
-            tickfont: {color: 'rgb(0, 0, 0)', size: 12},
-            showline: true,
-            showgrid: false,
-            range: [0, minVal],
-            autorange: "reversed"
-          },
-          xaxis: {
-            showline: false,
-            showgrid: true,
-            showticklabels: true,
-            tickformat: "%b-%d",
-            title: "",
-            titlefont: { color: "rgb(0, 0, 0)", size: 14 },
-            tickfont: { color: "rgb(0, 0, 0)", size: 12 },
-            //range: [0, 15]
-          },
-          height: 300,
-          showlegend: true,
-          legend: {x: 0, y: 0.95, xanchor: 'left', yanchor: 'right'},
-          margin: {t: 30, b: 30, r: 30, l: 50 },
-          shapes: [{
-            type: 'line', xref: 'paper', yref: 'y',
-            x0: 0, x1: 1, y0:0, y1: 0,
-            line: {color: "#745508", width: "2"}
-          }],
-          annotations: [
-            //above ground
-            { xref: 'paper', yref: 'paper', //ref is assigned to x values
-              x: 0.6, y: 1,
-              xanchor: 'left', yanchor: 'top',
-              text: "Land Surface", 
-              font: {family: 'verdana', size: 11, color: '#745508'},
-              showarrow: false,
-            }
-          ]
-        };
+    //draw 2022 year
+    var yCurrent = selGWMonthly
+      .filter(function (d) {
+        return d.year === currentYear;
+      })
+      .map(function (d) {
+        return d.mean_depth_ft;
+      });
+    var trace2022 = {
+      x: xMonth,
+     y: yCurrent,
+     mode: "lines+markers",
+      type: "scatter",
+      hovertemplate: "%{y:.1f} ft in %{x}, " + currentYear,
+      opacity: 1,
+      marker: { color: "black", size: 6 },
+      line: { color: "black", width: 3 },
+      name: currentYear,
+      showlegend: true,
+    };
+    data4.push(trace2022);
 
-        data.push(seltrace);
-        Plotly.newPlot("gwPlot4", data, gwlayout4, config);
-
-      } //end if we have utility data for selectect utility
-    }); // end D3
-} //END CREATE CHART FUNCTION ##########################################################
+    //PLOT CHART
+    var gwlayout4 = {
+      yaxis: {
+        title: "Feet below surface (ft)",
+        titlefont: {color: 'rgb(0, 0, 0)', size: 14},
+        tickfont: {color: 'rgb(0, 0, 0)', size: 12},
+        showline: true,
+        showgrid: false,
+        range: [0, minVal],
+        autorange: "reversed"
+      },
+      xaxis: {
+        showline: false,
+        showgrid: true,
+        showticklabels: true,
+        tickformat: "%b-%d",
+        title: "",
+        titlefont: { color: "rgb(0, 0, 0)", size: 14 },
+        tickfont: { color: "rgb(0, 0, 0)", size: 12 },
+      },
+      hovermode: "closest",
+      height: 300,
+      showlegend: true,
+      legend: {x: 0, y: 0.95, xanchor: 'left', yanchor: 'right'},
+      margin: {t: 30, b: 30, r: 10, l: 35 },
+      shapes: [{
+        type: 'line', xref: 'paper', yref: 'y',
+        x0: 0, x1: 1, y0:0, y1: 0,
+        line: {color: "#745508", width: "2"}
+      }],
+      annotations: [
+        //above ground
+        { xref: 'paper', yref: 'paper', //ref is assigned to x values
+          x: 0.6, y: 1,
+          xanchor: 'left', yanchor: 'top',
+          text: "Land Surface", 
+          font: {family: 'verdana', size: 11, color: '#745508'},
+          showarrow: false,
+        }
+      ]
+    };
+    Plotly.newPlot("gwPlot4", data4, gwlayout4, config);
+  }); // end D3
+} //END CREATE MONTHLY CHART FUNCTION ##########################################################
